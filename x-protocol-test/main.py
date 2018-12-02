3b006a0b853dbbb38be3dcbd2e7726d3c0e7e616
yamamo-i <sw.spc2010@gmail.com>
2018-12-02 16:09:33 +0900

    add a sample code.
    * CRUDやTransactionの発行を試してみるサンプルコードを追加
    * READMEの修正

--- x-protocol-test/main.py
+++ x-protocol-test/main.py
@@ -3,12 +3,39 @@
 import mysqlx
 
 # Connect to server on localhost
-session = mysqlx.get_session({'host': 'localhost', 'port': 33060, 'user': 'root', 'password': 'root'})
+session = mysqlx.get_session({'host': '127.0.0.1', 'port': 33060, 'user': 'root', 'password': 'root'})
 
-schema = session.get_schema('test')
+try:
+    schema = session.get_schema('mysql')
+    schema.create_collection('my_collection', True)
+    collection = schema.get_collection('my_collection')
 
-# Use the collection 'my_collection'
-schema.create_collection('my_collection', True)
-collection = schema.get_collection('my_collection')
+    # Start Transaction
+    session.start_transaction()
 
-session.close()
+    # INSERT
+    record_list = [{"_id": _id, "age": _id * 5} for _id in range(10)]
+    collection.add(*record_list).execute()
+
+    # READ
+    record = collection.find("age >= :param").bind("param", 21).limit(2).execute()
+    for rec in record.fetch_all():
+        print(rec)
+
+    # UPDATE
+    collection.modify("age <= 21") \
+        .patch('{"_is": "young"}').execute()
+    collection.modify("age > 21") \
+        .patch('{"_is": "old"}').execute()
+
+    # DELETE
+    collection.remove("age >= 31").execute()
+
+    # COMMIT
+    session.commit()
+
+except Exception:
+    # ROLLBACK
+    session.rollback()
+finally:
+    session.close()
